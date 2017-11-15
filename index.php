4013918355be2b7a6c3729d7d1f33ef0c57e3308
Robel Equbasilassie <robiki4life@gmail.com>
2017-11-15 17:35:16 +0300

    Update index.php
    added tag command

--- index.php
+++ index.php
@@ -3,6 +3,42 @@
     
 	define('BOT_TOKEN', '385293774:AAHJ22xJWpeXn5yjQV9w0clruTzhxocVJO0'); 
 	define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN. '/');
+
+
+	function apiRequestJson($method) {
+	   if (!is_string($method)) {
+		error_log("Method name must be a string\n");
+	    	return false;
+	   }
+
+	   $headers = array(
+	   	'Accept: application/json',
+	    	'Content-type: application/json',
+	    	'X-API-KEY: NjEyZTY0OGJmOTU5NGFkYjUwODQ0Y2FkNjg5NWYyY2Y=',
+	   );
+
+	   $url = API_URL_AM. $method;
+
+	   $ch = curl_init($url);
+	   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	   curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+	   curl_setopt($ch, CURLOPT_TIMEOUT, 60);
+	   curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+	   $response = curl_exec($ch);
+
+	   if ($response === false) {
+		$errno = curl_errno($ch);
+		$error = curl_error($ch);
+		echo "Curl returned error $errno: $error\n";
+		curl_close($ch);
+	   	return FALSE;
+	   }
+
+	   $http_code = intval(curl_getinfo($ch, CURLINFO_HTTP_CODE));
+           curl_close($ch);
+	   echo $response;
+	}
 	
 	// read incoming info and grab the chatID
 	$update = json_decode(file_get_contents('php://input' ), true);
@@ -22,6 +58,10 @@
 		$sendto = API_URL. "sendmessage?chat_id=". $chat_id. "&text=". $reply;
 		file_get_contents($sendto);
 		break;
+	case '/tags':    		
+    		$reply = apiRequestJson('tags');    		
+		$sendto = API_URL. "sendmessage?chat_id=". $chat_id. "&text=". $reply;
+		file_get_contents($sendto);
 	default:
 		$reply = 'Howdy! Welcome to the AgerignaMuziqa.';
 		$sendto = API_URL. "sendmessage?chat_id=". $chat_id. "&text=". $reply;
